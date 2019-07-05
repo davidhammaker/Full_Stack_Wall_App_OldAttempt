e6c0e80691ab488388cf2e76ec4581bb423c82b6
David J. Hammaker <davidjosh93@gmail.com>
2019-07-05 19:40:17 -0400

    Create 'Wall' component
    Add a simple component that will be used in making the wall app.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Wall from './components/Wall';
 
 export class App extends Component {
   render() {
     return (
       <div>
         <h1>Test</h1>
+        <Wall />
       </div>
     )
   }
---
+++ frontend/src/components/Wall.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class NewComponent extends Component {
+    render() {
+        return (
+            <div>
+                <p>Paragraph test.</p>
+            </div>
+        )
+    }
+}
+
+export default NewComponent
