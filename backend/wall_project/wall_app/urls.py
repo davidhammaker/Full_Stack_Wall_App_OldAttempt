37bf3c0671138839dd4d3b3b05ca2a5bdd086bb2
David J. Hammaker <davidjosh93@gmail.com>
2019-07-05 14:12:23 -0400

    Create URLs for viewing REST framework API
    URLs include 'post' URL for viewing Posts.

--- backend/wall_project/wall_app/urls.py
+++ backend/wall_project/wall_app/urls.py
@@ -1,5 +1,13 @@
 from django.urls import path, include
+from . import views
+from rest_framework import routers
+
+# Instantiate router:
+router = routers.DefaultRouter()
+
+# Register router with Post viewset using prefix 'posts'
+router.register('posts', views.PostView)
 
 urlpatterns = [
-
+    path('', include(router.urls))
 ]
