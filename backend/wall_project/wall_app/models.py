0ce79b90886e6182723924d3dfff600b682d1d07
David J. Hammaker <davidjosh93@gmail.com>
2019-07-05 13:42:24 -0400

    Create Post model
    Add model for representing message posts on the wall app.

--- backend/wall_project/wall_app/models.py
+++ backend/wall_project/wall_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
+
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    message = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        d = self.date_posted
+        return f'{self.author.username}_' \
+            f'{d.year}-{d.day}-{d.month}_{d.hour}:{d.minute}:{d.second}'
