2d788ea57945a948cc20e7429d0d1ecd57c742a3
David J. Hammaker <davidjosh93@gmail.com>
2019-07-05 19:48:10 -0400

    Add state dummy data
    Add hard-coded dummy data for development use.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,30 @@ import React, { Component } from 'react';
 import Wall from './components/Wall';
 
 export class App extends Component {
+
+  state = {
+    posts: [
+      {
+        id: 1,
+        author: 'davidjosh',
+        message: 'Post number 1.',
+        date_posted: '2019-07-05T23:44:30Z'
+      },
+      {
+        id: 2,
+        author: 'davidjosh',
+        message: 'Post number 2.',
+        date_posted: '2019-07-05T23:45:31Z'
+      },
+      {
+        id: 3,
+        author: 'davidjosh',
+        message: 'Post number 3.',
+        date_posted: '2019-07-05T23:46:32Z'
+      }
+    ]
+  }
+
   render() {
     return (
       <div>
