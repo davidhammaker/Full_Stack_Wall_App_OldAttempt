6c3e967bc88c63e02b754281706e399124fa5a79
David J. Hammaker <davidjosh93@gmail.com>
2019-07-05 22:04:38 -0400

    Replace dummy data with API
    Use data from backend, temporarily using the local host URL.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,19 @@
 import React, { Component } from 'react';
 import Wall from './components/Wall';
+import $ from 'jquery';
 
-export class App extends Component {
+class App extends Component {
 
   state = {
-    posts: [
-      {
-        id: 1,
-        author: 'davidjosh',
-        message: 'Post number 1.',
-        date_posted: '2019-07-05T23:44:30Z'
-      },
-      {
-        id: 2,
-        author: 'davidjosh',
-        message: 'Post number 2.',
-        date_posted: '2019-07-05T23:45:31Z'
-      },
-      {
-        id: 3,
-        author: 'davidjosh',
-        message: 'Post number 3.',
-        date_posted: '2019-07-05T23:46:32Z'
-      }
-    ]
+    posts: []
+  }
+
+  componentDidMount() {
+    let promise = $.get("http://127.0.0.1:8000/posts/");
+    promise.then(
+      data => this.setState({ posts: data }),
+      error => console.log('Error: ', error)
+    );
   }
 
   render() {
