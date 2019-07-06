dfb9e7820d3e05e6475ad882f4effe483ab33ad3
David J. Hammaker <davidjosh93@gmail.com>
2019-07-06 00:08:53 -0400

    Create user registration view
    Create 'register' view function, which will hopefully be able to take 
    POST requests and save users in the database.

--- backend/wall_project/users/views.py
+++ backend/wall_project/users/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from .forms import WallUserCreationForm
 
-# Create your views here.
+
+def register(request):
+    if request.method == 'POST':
+        form = WallUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+    return HttpResponse('OK')
--- backend/wall_project/wall_project/urls.py
+++ backend/wall_project/wall_project/urls.py
@@ -2,11 +2,13 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import (
     TokenObtainPairView, TokenRefreshView)
+from users import views as user_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('wall_app.urls')),
     path('api-auth/', include('rest_framework.urls')),
     path('api/token/', TokenObtainPairView.as_view()),
-    path('api/token/refresh/', TokenRefreshView.as_view())
+    path('api/token/refresh/', TokenRefreshView.as_view()),
+    path('register/', user_views.register, name='register')
 ]
